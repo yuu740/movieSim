056b0979f925a722de4bf9fb663d39a6ecf62bed
Revel <revelcahyadi2004@gmail.com>
2025-07-01 19:14:51 +0700

    feat (movies) : make the movies model

---
+++ app/Models/Movies.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movies extends Model
+{
+    public function episodes() {
+        return $this->hasMany(Episodes::class);
+    }
+}
