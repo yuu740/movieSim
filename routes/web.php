6fcf3ccab872ffa2610fb48659e534b573d2dd34
Revel <revelcahyadi2004@gmail.com>
2025-07-01 19:12:47 +0700

    feat (route) : make route for createMovie

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/', [MovieController::class, 'index']);
+Route::post('/', [MovieController::class, 'createMovie']);
