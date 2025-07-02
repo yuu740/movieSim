3bd5976c71acbe510a06353e3870cd376411fce6
Revel <revelcahyadi2004@gmail.com>
2025-07-02 16:15:07 +0700

    feat (controller) : make crud controller for movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -16,8 +16,11 @@ class MovieController extends Controller
     public function createMovie(Request $req) {
         $file = $req->file('image');
         $imageName = time().'.'.$file->getClientOriginalExtension();
-        Storage::putFileAs('public/images', $file, $imageName);
-        $imageName = 'images/'.$imageName;
+        // Storage::disk('public')->putFileAs('images', $file, $imageName);
+        Storage::putFileAs('images', $file, $imageName);
+        // Storage::putFileAs('storage/app/private/public/images', $file, $imageName);
+
+        $imageName = 'storage/images/'.$imageName;
         $movie = new Movies();
         $movie->title = $req->title;
         $movie->description = $req->description; //based from name on view
@@ -27,5 +30,48 @@ class MovieController extends Controller
 
         return redirect()->back();
 
+    }
+
+    public function updateMovie(Request $req) {
+        $file = $req->file('image');
+
+
+
+        $movie = Movies::find($req->id);
+
+
+
+        $movie->title = $req->title != null ? $req->title : $movie->title;
+        $movie->description = $req->description != null ? $req->description : $req->description; //based from name on view
+
+        if ($file != null) {
+            $imageName = time().'.'.$file->getClientOriginalExtension();
+            // Storage::disk('public')->putFileAs('images', $file, $imageName);
+            Storage::putFileAs('images', $file, $imageName);
+            // Storage::putFileAs('storage/app/private/public/images', $file, $imageName);
+
+            $imageName = 'storage/images/'.$imageName;
+            Storage::delete('public/'.$movie->image);
+
+        }
+        else {
+            $movie->image = $movie->image;
+        }
+        $movie->image = $imageName;
+
+        $movie->save();
+
+        return redirect()->back();
+
+    }
+
+    public function deleteMovie($id) {
+        $movie = Movies::find($id);
+
+        if (isset($movie)) {
+            Storage::delete('public/'.$movie->image);
+            $movie->delete();
+        }
+        return redirect()->back();
     }
 }
