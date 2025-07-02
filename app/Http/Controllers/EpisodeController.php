fa9b2149ccc6b05e5c92f50d46b475723e00fb77
Revel <revelcahyadi2004@gmail.com>
2025-07-02 16:56:43 +0700

    feat (controller) L add validation for episode

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Episodes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EpisodeController extends Controller
 {
     //
     public function createEpisode(Request $req) {
+        $rules = [
+            'movieID' => 'required|exists:movies,id',
+            'title' => 'required',
+            'episodes' => 'required|starts_with:Episode'
+        ];
+
+        $validator = Validator::make($req->all(), $rules);
+
+        if($validator->fails()) {
+            return back()->withErrors($validator);
+        }
         $episodes = new Episodes();
         $episodes->movie_id = $req->movieID;
         $episodes->episodes = $req->episodes;
