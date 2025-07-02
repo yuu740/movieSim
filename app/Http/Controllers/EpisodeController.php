6713a41b15272196a58de4bd3b065271c4ca9bb0
Revel <revelcahyadi2004@gmail.com>
2025-07-02 16:15:24 +0700

    feat (controller) : make the controller for episode mapper

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Episodes;
 use Illuminate\Http\Request;
 
 class EpisodeController extends Controller
 {
     //
+    public function createEpisode(Request $req) {
+        $episodes = new Episodes();
+        $episodes->movie_id = $req->movieID;
+        $episodes->episodes = $req->episodes;
+        $episodes->title = $req->title;
+
+        $episodes->save();
+
+        return redirect()->back();
+    }
 }
